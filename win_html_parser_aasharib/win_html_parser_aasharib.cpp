8cb067d3286742a0dcf8ac877016b474e22b2ef1
MuhammadAasharib <aasharib@icloud.com>
2016-12-24 23:55:39 +0500

    1.0 Successful compilation of myhtml library with visual studio library. coding should be done only in main function without affecting other files

--- win_html_parser_aasharib/win_html_parser_aasharib.cpp
+++ win_html_parser_aasharib/win_html_parser_aasharib.cpp
@@ -2,10 +2,27 @@
 //
 
 #include "stdafx.h"
-
+#include "myhtml/myhtml.h"
 
 int main()
 {
-    return 0;
-}
 
+	// sample code
+	char html[] = "<div><span>HTML</span></div>";
+
+	// basic init
+	myhtml_t* myhtml = myhtml_create();
+	myhtml_init(myhtml, MyHTML_OPTIONS_DEFAULT, 1, 0);
+
+	// first tree init 
+	myhtml_tree_t* tree = myhtml_tree_create();
+	myhtml_tree_init(tree, myhtml);
+
+	// parse html
+	myhtml_parse(tree, MyHTML_ENCODING_UTF_8, html, strlen(html));
+
+	// release resources
+	myhtml_tree_destroy(tree);
+	myhtml_destroy(myhtml);
+	return 0;
+}
